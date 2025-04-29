4fa25952d3ba5c97543ea5de90f9b19d359a41e3
PEDRO HENRIQUE FERNANDES JORGE <24122555@dsn127m1242268.cfp127aluno.local>
2025-04-29 10:10:54 -0300

    adicionando a terceira tela

--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/StudentsScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/StudentsScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -26,10 +28,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -38,6 +42,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.lionschool.R
 import br.senai.sp.jandira.lionschool.screens.components.CardCurso
+import br.senai.sp.jandira.lionschool.screens.components.CardStudent
 import br.senai.sp.jandira.lionschool.screens.components.Logo
 
 @Composable
@@ -115,19 +120,149 @@ fun StudentsScreen(modifier: Modifier = Modifier){
                     color = Color.Gray
                 )
             )
-            Column {
-                Row {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp),
+                horizontalAlignment = Alignment.Start
+            ){
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 20.dp),
+                ) {
                     Card (
                         modifier = Modifier
-                            .width(30.dp)
-                            .height(15.dp),
+                            .width(80.dp)
+                            .height(30.dp),
+                        shape = RoundedCornerShape(20.dp),
+                        colors = CardDefaults.cardColors(colorResource(R.color.azul_logo))
                     ){
-                        Text(
-                            text = stringResource(R.string.todos),
-                            color = Color.White,
-                            fontSize = 10.sp
-                        )
+                        Row(
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .padding(start = 10.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ){
+                            Text(
+                                text = stringResource(R.string.todos),
+                                color = Color.White,
+                                fontSize = 15.sp
+                            )
+                        }
                     }
+                    Card (
+                        modifier = Modifier
+                            .width(105.dp)
+                            .height(30.dp)
+                            .padding(start = 10.dp),
+                        shape = RoundedCornerShape(30.dp),
+                        colors = CardDefaults.cardColors(colorResource(R.color.amarelo))
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .padding(start = 10.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ){
+                            Text(
+                                text = stringResource(R.string.cursando),
+                                color = colorResource(R.color.azul_logo),
+                                fontSize = 15.sp
+                            )
+                        }
+                    }
+                    Card (
+                        modifier = Modifier
+                            .width(105.dp)
+                            .height(30.dp)
+                            .padding(start = 10.dp),
+                        shape = RoundedCornerShape(30.dp),
+                        colors = CardDefaults.cardColors(colorResource(R.color.amarelo))
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .padding(start = 10.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ){
+                            Text(
+                                text = stringResource(R.string.finalizado),
+                                color = colorResource(R.color.azul_logo),
+                                fontSize = 15.sp
+                            )
+                        }
+                    }
+                }
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.graduation),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(30.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.students_list),
+                        color = colorResource(R.color.azul_logo),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(top = 20.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
+                    CardStudent(
+                        nome = "Luana Oliveira Dias",
+                        documento = "20151001018",
+                        anoTermino = "2022",
+                        foto = painterResource(R.drawable.ellipse)
+                    )
                 }
             }
         }
